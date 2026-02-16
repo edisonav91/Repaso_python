0ae5e743898b59eca0a13df813d3d17f212b9c56
Edison Alvarez <edisonav1991@gmail.com>
2026-02-16 14:24:52 -0500

    Sentencia if-elif-else

---
+++ sentencia_if_elif_else.py
@@ -0,0 +1,9 @@
+print('***Sentencias if elifelse en Python***')
+dia_lluvioso = False
+dia_nublado = False
+if dia_lluvioso:
+    print('Llevar paraguas')    
+elif dia_nublado:
+    print('llevar impermeable')
+else:
+    print('Dejar paraguas e impermeable en casa')
