970f9d7cc56ae70cb5f320c1f3be9058d0bda55a
Edison Alvarez <edisonav1991@gmail.com>
2026-02-16 12:22:31 -0500

    operadores de comparación

---
+++ operadores_comparacion.py
@@ -0,0 +1,30 @@
+print('***Operadores de comparacion o relacionales en Python***')
+
+a, b = 6, 5
+print(f'Valor de a: {a}')
+print(f'Valor de b: {b}')       
+
+#Operador de igualdad == devuelven true o folse
+resultado = a == b
+print(f'¿a es igual a b? {resultado}')
+
+# Operador de diferente != devuelven true o false
+resultado = a != b
+print(f'¿a es diferente de b? {resultado}')
+
+#Operdaor de mayor que > devuelven true o false
+resultado = a > b
+print(f'¿a es mayor que b? {resultado}')
+
+#Operador de menor que < devuelven true o false
+resultado = a < b          
+print(f'¿a es menor que b? {resultado}') 
+
+#Operador de mayor o igual que >= devuelven true o false
+resultado = a >= b      
+print(f'¿a es mayor o igual que b? {resultado}')
+
+#operador menor o igual que <= devuelven true o false
+resultado = a <= b          
+print(f'¿a es menor o igual que b? {resultado}')
+
