4b8aae42ead5185b58097329f452fbeb6a44b29a
Edison Alvarez <edisonav1991@gmail.com>
2026-02-17 21:54:38 -0500

    Acceder a indices de una lista y modificarlos

--- listas_tuplas_dicc_funciones/listas.py
+++ listas_tuplas_dicc_funciones/listas.py
@@ -1,9 +1,29 @@
 print('***Listas en python***')
 
 nombres = ['Carla', 'Juan', 'Laura']
-print(F'Lista de nombres = {nombres}')
+#print(F'Lista de nombres = {nombres}')
 
 # Tambien podemos tener una lista heterogenea con diferentes tipos de datos
 
 lista_heterogenea = [100, False, 'Edison']
-print(lista_heterogenea)
+#print(lista_heterogenea)
+
+# Interar o leer cada elemento de la lista
+for nombre in nombres:
+    #print(nombre, end = ' ') # end = '' para qeu imprima en la misma linea
+
+# Lista de numeros
+
+    numeros = [100, 200, 300, 400, 500]
+
+ #Recuperar valore de indice de una lista
+
+print(numeros [1])
+
+print(f' Indice numero 3 de la lista es: {numeros[3]}')
+
+
+# Modificar los indices de una lista 
+
+numeros[0] = 1000
+numeros[1] = 1000
