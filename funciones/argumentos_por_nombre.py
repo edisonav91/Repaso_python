c51a27eb24daaadbd2f4afc42ce6ea2ae53cbf70
Edison Alvarez <edisonav1991@gmail.com>
2026-02-20 16:26:49 -0500

    Funcion de argumentos por nombre

---
+++ funciones/argumentos_por_nombre.py
@@ -0,0 +1,10 @@
+print('***Funcion de argumentos pornombre***')
+
+def crear_persona(nombre, apellido='', edad=0):
+    print(f'Persona: nombre = {nombre} apellido: {apellido} edad: {edad}')
+
+# LLmar a nuestra funcion 
+# crear_persona('Anderson', 'Alvarez', 34)
+
+crear_persona(nombre='Anderson', edad=34)
+
